ed5b110b00ae75ab7c5103fcb4a85c8b3b514dcc
Elrich Herman <juniorherman1gmail.com>
2021-08-11 00:29:43 +0200

    Update

---
+++ src/main/java/za/ac/cput/services/ClientAccountService.java
@@ -0,0 +1,64 @@
+package za.ac.cput.services;
+
+/*
+   @Subject: Project 3
+   @Description:ClientAccountService.java
+   @Author: Elrich Herman
+   @Student Number: 218150040
+   @Date: 07 August 2021
+*/
+
+import za.ac.cput.entity.ClientAccount;
+import za.ac.cput.repository.clientAccount.ClientAccountRepository;
+import za.ac.cput.services.clientAccount.IClientAccountService;
+import java.util.Set;
+
+public class ClientAccountService implements IClientAccountService
+{
+    private static  ClientAccountService service = null;
+    private ClientAccountRepository repository = null;
+
+    private ClientAccountService()
+    {
+        this.repository = ClientAccountRepository.getRepository();
+    }
+
+    public static ClientAccountService getService()
+    {
+        if(service == null){
+            service = new ClientAccountService();
+        }
+        return service;
+    }
+
+    @Override
+    public ClientAccount create(ClientAccount clientAccount)
+    {
+        return this.repository.create(clientAccount);
+    }
+
+    @Override
+    public ClientAccount read(String clientID)
+    {
+        return this.repository.read(clientID);
+    }
+
+    @Override
+    public ClientAccount update(ClientAccount clientAccount)
+    {
+        return  this.repository.update(clientAccount);
+    }
+
+    @Override
+    public boolean delete(String equipmentStorageId)
+    {
+        return this.repository.delete(equipmentStorageId);
+    }
+
+    @Override
+    public Set<ClientAccount> getAll()
+    {
+        return (Set<ClientAccount>) this.repository.getAll();
+    }
+
+}
---
+++ src/main/java/za/ac/cput/services/LoginService.java
@@ -0,0 +1,64 @@
+package za.ac.cput.services;
+
+/*
+   @Subject: Project 3
+   @Description:LoginService.java
+   @Author: Elrich Herman
+   @Student Number: 218150040
+   @Date: 07 August 2021
+*/
+
+import za.ac.cput.entity.Login;
+import za.ac.cput.repository.login.LoginRepository;
+import za.ac.cput.services.login.features.queries.ILoginService;
+import java.util.Set;
+
+public class LoginService implements ILoginService
+{
+    private static  LoginService service = null;
+    private LoginRepository repository = null;
+
+    private LoginService()
+    {
+        this.repository = LoginRepository.getRepository();
+    }
+
+    public static LoginService getService()
+    {
+        if(service == null){
+            service = new LoginService();
+        }
+        return service;
+    }
+
+    @Override
+    public Login create(Login login)
+    {
+        return this.repository.create(login);
+    }
+
+    @Override
+    public Login read(String loginID)
+    {
+        return this.repository.read(loginID);
+    }
+
+    @Override
+    public Login update(Login login)
+    {
+        return  this.repository.update(login);
+    }
+
+    @Override
+    public boolean delete(String loginID)
+    {
+        return this.repository.delete(loginID);
+    }
+
+    @Override
+    public Set<Login> getAll()
+    {
+        return (Set<Login>) this.repository.getAll();
+    }
+
+}
