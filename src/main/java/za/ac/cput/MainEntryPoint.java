0dded5221fb6c4f123239d45a81a1fc79d629aec
TyronneLloyd <215141210@mycput.ac.za>
2021-08-30 19:04:50 +0200

    ControllerTest

--- src/main/java/za/ac/cput/MainEntryPoint.java
+++ src/main/java/za/ac/cput/MainEntryPoint.java
@@ -7,13 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import za.ac.cput.ui.dashboard.admin.UIAdminDashboard;
+import za.ac.cput.ui.equipment.UIEquipment;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 public class MainEntryPoint {
     public static void main(String[] args) {
-        new UIAdminDashboard();
+        new UIEquipment();
         SpringApplication.run(MainEntryPoint.class, args);
     }
 
--- src/test/java/za/ac/cput/services/EmployeeServiceTest.java
+++ src/test/java/za/ac/cput/services/EmployeeServiceTest.java
@@ -1,6 +1,4 @@
-
 package za.ac.cput.services;
-
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -11,52 +9,36 @@ import za.ac.cput.entity.Employee;
 import za.ac.cput.factory.EmployeeFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @TestMethodOrder(MethodOrderer.MethodName.class)
 @SpringBootTest
 class EmployeeServiceTest {
     @Autowired
     private  EmployeeService service;
-    private static Employee employee = EmployeeFactory.createEmployee(
-            "Ty",
-            "Lloyd",
-            "Ty@gmail.com",
-            215141210,
-            "1");
-
+    private static Employee employee = EmployeeFactory.createEmployee("Ty", "Lloyd", "Ty@gmail.com", 215141210, "1");
     @Test
     void a_create() {
         Employee create = service.create(employee);
         assertEquals(create.getEmployeeNumber(), employee.getEmployeeNumber());
-        System.out.println("Created: " + create);
-    }
-
+        System.out.println("Created: " + create);}
     @Test
     void b_read() {
         Employee read = service.read(employee.getEmployeeNumber());
         assertNotNull(read);
-        System.out.println("Read: " + read);
-    }
-
+        System.out.println("Read: " + read);}
     @Test
     void c_update() {
-        Employee update = new Employee.Builder().copy(employee).setName("Jeff").build();
+        Employee update = new Employee.Builder().copy(employee).setName("Tyronne").build();
         assertNotNull(service.update(update));
-        System.out.println("Updated: " + update);
-    }
-
+        System.out.println("Updated: " + update);}
     @Test
     void d_getAll() {
         System.out.println("Display All:");
-        System.out.println(service.getAll());
-    }
-
+        System.out.println(service.getAll());}
     @Test
     void e_delete() {
         boolean success = service.delete(employee.getEmployeeNumber());
         assertTrue(success);
         System.out.println("Delete: " + success);
     }
-
 }
 
